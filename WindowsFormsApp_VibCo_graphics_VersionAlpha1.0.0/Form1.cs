83b331338cb68893aff5c2257f7a798f341570a3
shensitt <90465081+Shensitt@users.noreply.github.com>
2022-02-09 21:48:56 +0300

    Add files via upload
    v alpha 1.0.0
    changelog: Initial build; 
    Features: 1) left mouse click to show cursor coordinates;
    2) right mouse click to clean the page

---
+++ WindowsFormsApp_VibCo_graphics_VersionAlpha1.0.0/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            if (e.Button == MouseButtons.Left)
+            {
+                int CursorX = Cursor.Position.X;
+                int CursorY = Cursor.Position.Y;
+
+                string s = CursorX.ToString();
+                string s1 = CursorY.ToString();
+
+                string ss = s + "," + s1;
+                g.DrawString(ss, new Font("Times New Roman", 8), new SolidBrush(Color.Black), new Point(CursorX, CursorY));
+
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Cleaning...","Right mouse button is down");
+                g.Clear(Color.White);
+            }
+
+        }
+
+      
+    } 
+}
+
