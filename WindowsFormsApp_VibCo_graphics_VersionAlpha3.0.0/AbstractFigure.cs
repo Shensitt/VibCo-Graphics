3ead8e587a0e7f0f0375802611bcd5ed7c781c48
shensitt <90465081+Shensitt@users.noreply.github.com>
2022-03-15 22:54:25 +0300

    Add files via upload
    added 2 classes, abstract for future different figures to draw and a class for drawing rectangles

---
+++ WindowsFormsApp_VibCo_graphics_VersionAlpha3.0.0/AbstractFigure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+     abstract class AbstractFigure
+    {
+       
+        public abstract void DrawFigureCordPoint1(int x0,int y0);
+        public abstract void DrawFigureCordPoint2(int x1, int y1);
+
+        public abstract void Draw(Graphics g);
+
+        public abstract void DrawDash(Graphics g);
+
+        public abstract void Hide(Graphics g);
+
+    }
+}
---
+++ WindowsFormsApp_VibCo_graphics_VersionAlpha3.0.0/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   
+   
+    public partial class Form2 : Form
+    {
+       
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        bool draw_frag;
+        int X0;
+        int Y0;
+        int X1;
+        int Y1;
+       
+        public void Form2_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            Graphics g = CreateGraphics();
+            if (e.Button == MouseButtons.Left)
+            {
+               draw_frag = true;
+                
+                X0 = e.X;
+                Y0 = e.Y;
+            
+                var rect = new Rectangl();
+                rect.DrawFigureCordPoint1(X0, Y0);
+               
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Cleaning...", "Right mouse button is down");
+                g.Clear(Color.White);
+            }
+        }
+
+        private void Form2_MouseMove(object sender, MouseEventArgs e)
+        {
+           X1 = e.X - X0;
+            Y1 = e.Y - Y0;
+
+            if (draw_frag == true)
+            {
+                Graphics g = CreateGraphics();
+                var rect = new Rectangl();
+                rect.DrawFigureCordPoint1(X0, Y0);
+                rect.DrawFigureCordPoint2(X1, Y1);
+                rect.DrawDash(g);
+               
+            }
+        }
+
+        private void Form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (draw_frag == true)
+            {
+                Graphics g = CreateGraphics();
+
+                var rect = new Rectangl();
+
+                rect.DrawFigureCordPoint1(X0, Y0);
+                rect.DrawFigureCordPoint2(X1, Y1);
+                rect.Draw(g);
+
+            }
+            draw_frag = false;
+        }
+      
+
+    }
+}
